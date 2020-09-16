93d96bb89fa72e6ec918805ce7a54e73592110b9
survey <zacharysigle084@gmail.com>
2020-09-16 00:25:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var userName = prompt('What is your name?');
+
+if (userName === 'Zachary'){
+  alert(' i love your name it is mine too');
+} else {
+  alert ('That is a nice name');
+}
+
+var questions = prompt('Do you like to code or are you new to coding');
+
+if (questions === 'i like to code'){
+  alert('Great I hope this can help you get better at coding')
+} else{
+  alert('Well welcome to coding and I hope this can help you get to know how to code')
+}
+
+var football = prompt('What is your favorite football team');
+
+if (football === 'rams'){
+  alert('RAMS SUCK')
+} else{
+  alert('Hope your team does good this season')
+}
+var Grades = prompt('What are your grades so far don´t lie only you can see this');
+
+if (Grades === 'a'){
+  alert('Great job keep up the work!');
+} else{
+  alert('You can get an A if you try so try and get it you will fell a lot better');
+} 
